ef854a5b46378143fcbc654d5a48b181d2de2566
Hemanth Reddy <kollahemanthreddy@gmail.com>
2026-07-16 19:40:38 +0530

    chore : learned npm package commands

---
+++ notes/npm-packages/package.js
@@ -0,0 +1,36 @@
+//BUILT IN PACKAGES
+// const fs = require("fs");
+// const path = require("path");
+// const os = require("os");
+// const http = require("http");
+
+//LOCAL PACKAGES-Installed only for the current project.
+// npm install express.
+// project/
+// │
+// ├── node_modules/
+// ├── package.json
+// ├── package-lock.json
+// └── app.js
+
+//GLOBAL PACKAGES-Installed for the entire computer.
+//installation-npm install -g nodemon
+//run-nodemon app.js
+
+//MOST COMMON NPM COMMANDS
+//initalize a project=npm init or npm init -y
+
+//install a package=npm install express
+//short form=npm i express
+
+//install multiple packages=npm install express mongoose dotenv
+
+//install globally=npm install -g nodemon
+
+//remove a package=npm uninstall express
+
+//update a package=npm update express
+
+//view installed packages=npm list
+
+//view global packages=npm list -g --depth=0
